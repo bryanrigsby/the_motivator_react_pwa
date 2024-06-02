e6af764d7a59103de6afc7e4c0899dd5c8297dc6
Bryan Rigsby <bryanrigsby@att.net>
2024-06-01 20:21:04 -0500

    update

---
+++ the_motivator/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import logo from './assets/images/the_motivator_icon_192x192.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
